9af4e4d898650154ba1ad543c2e33671968d70c8
swapnilabane <swapnil143a@gmail.com>
2024-01-18 13:46:49 +0530

    Create Header, footer, Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Write from './pages/Write';
+import Header from './components/Header';
+import Footers from './components/Footers';
+
 function App() {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <div className='flex flex-col min-h-screen'>
+        <main className='flex-grow'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/write' element={<Write />} />
+          </Routes>
+        </main>
+        <Footers />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,23 @@
+import Categories from '../components/Categories';
+import Posts from '../components/Posts';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='relative md:mx-36'>
+      <img src='laptop1.jpg' alt='laptop' className='w-full h-[38rem]' />
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white'>
+        <h1 className='text-4xl md:text-6xl font-bold mb-4'>
+          Discover, Learn, and Share Amazing Stories
+        </h1>
+        <h3 className='text-lg md:text-xl'>
+          Dive into a collection of thought-provoking articles, compelling
+          stories, and expert perspectives. Unleash your curiosity and discover
+          a wealth of knowledge waiting to be explored.
+        </h3>
+      </div>
+      <Posts />
+      <Categories />
+    </div>
+  );
 };
 export default Home;
