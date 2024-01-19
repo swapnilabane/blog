657fe874caf32c4ab6692b42b809e060a961a997
swapnilabane <swapnil143a@gmail.com>
2024-01-19 19:50:01 +0530

    create all routes and file upload

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,20 @@
-import SinglePage from './SinglePage';
-
 const About = () => {
   return (
-    <div>
-      <SinglePage />
+    <div className='flex items-center justify-center h-screen'>
+      <div className='max-w-md p-6 bg-white rounded-md shadow-md'>
+        <p className='text-center'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+          obcaecati! Repellat magnam ut commodi quia in, tempora ullam
+          exercitationem sequi illo consequuntur ipsam natus libero molestias
+          officiis pariatur, aspernatur neque a architecto odio magni aliquam
+          tempore blanditiis. Culpa quaerat ipsam quasi itaque, quos eveniet
+          quia cupiditate adipisci eius accusantium soluta.ue a architecto odio
+          magni aliquam tempore blanditiis. Culpa quaerat ipsam quasi itaque,
+          quos eveniet quia cupiditate adipisci eius accusantium soluta.
+        </p>
+      </div>
     </div>
   );
 };
+
 export default About;
--- server/index.js
+++ server/index.js
@@ -3,10 +3,20 @@ dotenv.config();
 import express from 'express';
 import mongoose from 'mongoose';
 import { authRoute } from './routes/authRoutes.js';
+import { userRoute } from './routes/userRoutes.js';
+import { postRoute } from './routes/postRoutes.js';
+import { categoryRoute } from './routes/categoryRoutes.js';
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use('/images', express.static(path.join(__dirname, '/images')));
 
 const port = process.env.PORT || '8000';
 const mongoURL = process.env.MONGO_URL;
@@ -21,7 +31,24 @@ const connectDB = async () => {
   }
 };
 
-app.use('/api/auth/v1', authRoute);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+});
+
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/user', userRoute);
+app.use('/api/v1/post', postRoute);
+app.use('/api/v1/categories', categoryRoute);
 
 connectDB().then(() => {
   app.listen(port, () => {
