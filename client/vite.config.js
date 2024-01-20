cfe358a3e3b359507eb5dab3391fdbe618ef99ad
swapnilabane <swapnil143a@gmail.com>
2024-01-20 22:34:15 +0530

    deploy edit 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://blog-two-lime-23.vercel.app',
+        target: 'http://localhost:3000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
